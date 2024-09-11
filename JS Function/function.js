16713e99eb3d8ef94b9ec7a25941e328b3aa6d15
md sium <55883376+mdsium@users.noreply.github.com>
2024-09-11 23:53:40 +0600

    Update function.js

--- JS Function/function.js
+++ JS Function/function.js
@@ -7,3 +7,7 @@ function myFunction(a, b) {
 //After a function expression has been stored in a variable, the variable can be used as a function:
 const x = function (a, b) {return a * b};
 let z = x(4, 3);
+
+//The Function() Constructor
+const myFunction = new Function("a", "b", "return a * b");
+let A = myFunction(4, 3);
