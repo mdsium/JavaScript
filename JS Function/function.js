a4d46141b15af822c06cf5276edb1b34c51e1aa2
md sium <55883376+mdsium@users.noreply.github.com>
2024-09-11 23:46:57 +0600

    Update function.js

--- JS Function/function.js
+++ JS Function/function.js
@@ -3,3 +3,7 @@ b=5;
 function myFunction(a, b) {
   return a * b;
 }
+
+//After a function expression has been stored in a variable, the variable can be used as a function:
+const x = function (a, b) {return a * b};
+let z = x(4, 3);
