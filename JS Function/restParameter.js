9e0162f9556a1e30a8d17f83baa79d732eb891df
md sium <55883376+mdsium@users.noreply.github.com>
2024-09-18 00:00:16 +0600

    Update restParameter.js

--- JS Function/restParameter.js
+++ JS Function/restParameter.js
@@ -19,6 +19,20 @@ function myFunction(a, b) {
   return a * b;
 }
 
+// The Arguments Object
+
+x = findMax(1, 123, 500, 115, 44, 88);
+
+function findMax() {
+  let max = -Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > max) {
+      max = arguments[i];
+    }
+  }
+  return max;
+}
+
 A = sumAll(1, 123, 500, 115, 44, 88);
 
 function sumAll() {
