61e491405cabb3248f3c59191d3b6092188e60d8
md sium <55883376+mdsium@users.noreply.github.com>
2024-10-10 23:29:34 +0600

    JavaScript object
    create JavaScript object

---
+++ object/CObject.js
@@ -0,0 +1,11 @@
+// Create an Object:
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
+
+// Display Data from the Object:
+document.getElementById("demo").innerHTML =
+person.firstName + " is " + person.age + " years old.";
